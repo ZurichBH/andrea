0e8a50072e045d2ddf1666c940917930fb2f4531
ascanzio <andyscanzio@public-docking-pat-hpx-3926.ethz.ch>
2014-03-31 11:08:42 +0200

    Semester Project

---
+++ semproj_aplpy.py
@@ -0,0 +1,358 @@
+import matplotlib
+matplotlib.use('Agg')
+import aplpy
+import os
+import pyfits
+import pywcs
+import astropy.cosmology
+import astropy.units as u
+
+
+def coord_source(ID):
+    os.chdir('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/Data/'
+             + ID+'/repro/')
+    with open('source.reg', 'r') as f:
+        lines = f.read()
+    ans = lines.find('circle')
+    ans1 = ans+len('circle(')
+    if lines[ans1+8] == ',':
+        xpix = float(lines[ans1:ans1+8])
+        if lines[ans1+17] == ',':
+            ypix = float(lines[ans1+9:ans1+17])
+        else:
+            ypix = float(lines[ans1+9:ans1+18])
+    else:
+        xpix = float(lines[ans1:ans1+9])
+        if lines[ans1+18] == ',':
+            ypix = float(lines[ans1+10:ans1+18])
+        else:
+            ypix = float(lines[ans1+10:ans1+19])
+    return xpix,  ypix
+
+
+def coord_source_1(ID):
+    os.chdir('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/Data/'
+             + ID + '/repro/')
+    with open('source_1.reg', 'r') as f:
+        lines = f.read()
+    ans = lines.find('circle')
+    ans1 = ans+len('circle(')
+    if lines[ans1+8] == ',':
+        xpix = float(lines[ans1:ans1+8])
+        if lines[ans1+17] == ',':
+            ypix = float(lines[ans1+9:ans1+17])
+        else:
+            ypix = float(lines[ans1+9:ans1+18])
+    else:
+        xpix = float(lines[ans1:ans1+9])
+        if lines[ans1+18] == ',':
+            ypix = float(lines[ans1+10:ans1+18])
+        else:
+            ypix = float(lines[ans1+10:ans1+19])
+    return xpix,  ypix
+
+
+def coord_source_2(ID):
+    os.chdir('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/Data/'
+             + ID + '/repro/')
+    with open('source_2.reg', 'r') as f:
+        lines = f.read()
+    ans = lines.find('circle')
+    ans1 = ans+len('circle(')
+    if lines[ans1+8] == ',':
+        xpix = float(lines[ans1:ans1+8])
+        if lines[ans1+17] == ',':
+            ypix = float(lines[ans1+9:ans1+17])
+        else:
+            ypix = float(lines[ans1+9:ans1+18])
+    else:
+        xpix = float(lines[ans1:ans1+9])
+        if lines[ans1+18] == ',':
+            ypix = float(lines[ans1+10:ans1+18])
+        else:
+            ypix = float(lines[ans1+10:ans1+19])
+    return xpix,  ypix
+
+
+def plot_xray(ID, z, dist, name):
+    print ID
+    os.chdir('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/Data/'
+             + ID + '/repro/')
+    gc = aplpy.FITSFigure('aplpy_'+ID+'_0.5-8_flux.img')
+    gc.show_colorscale(cmap='gist_heat', vmin=0, vmax=1.0e-07,
+                       stretch='arcsinh', smooth=1)
+    gc.show_regions('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/'
+                    + 'Data/'+ID+'/repro/source.reg')
+    gc.show_regions('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/'
+                    + 'Data/'+ID+'/repro/source_ext.reg')
+    gc.show_regions('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/'
+                    + 'Data/'+ID+'/repro/back.reg')
+
+    radius = dist*u.kpc*astropy.cosmology.arcsec_per_kpc_comoving(z)/u.arcsec
+    radius = radius.value
+    print radius
+    if radius < 60.0:
+        radius = 60.0
+    elif radius > 150.0:
+        radius = 150.0
+    radius += 5.0
+
+    scale = 10.0*u.kpc/astropy.cosmology.arcsec_per_kpc_comoving(z)
+    scale = scale.value
+    gc.add_label(0.20, 0.1, 'ObsID: '+ID+'\n'+name, relative=True,
+                 weight='heavy', size='x-large', color='gray')
+
+    xpix, ypix = coord_source(ID)
+    hdulist = pyfits.open('/Users/andyscanzio/Documents/ETH/Semestri/'
+                          + 'FS2014/Project/Data/'+ID+'/repro/acisf'
+                          + ID+'_repro_evt2.fits')
+    wcs = pywcs.WCS(hdulist[1].header)
+    print wcs.wcs.name
+    wcs.wcs.print_contents()
+    radec = wcs.wcs_pix2sky([[xpix, ypix]], 0)
+    print radec
+    ra, dec = radec[0][0], radec[0][1]
+    print ra, dec
+
+    gc.recenter(ra, dec, radius=radius/3600.0)
+    gc.tick_labels.set_xformat('ddmmss')
+    gc.tick_labels.set_yformat('ddmmss')
+    gc.add_scalebar(10.0/3600.0)
+    gc.add_colorbar()
+    gc.scalebar.set_label('10.0"/'+str(scale)[0:4]+' kpc')
+    gc.scalebar.set_font(size='x-large', weight='heavy')
+    gc.scalebar.set_color('gray')
+    gc.save('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/'
+            + 'Project/Images/'+ID+'_xray.eps')
+    gc.close()
+
+
+def plot_xray_1(ID, z, dist, name):
+    print ID
+    os.chdir('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/Data/'
+             + ID+'/repro/')
+    gc = aplpy.FITSFigure('aplpy_'+ID+'_0.5-8_flux.img')
+    gc.show_colorscale(cmap='gist_heat', vmin=0, vmax=1.0e-07,
+                       stretch='arcsinh', smooth=1)
+    gc.show_regions('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/'
+                    + 'Data/'+ID+'/repro/source_1.reg')
+    gc.show_regions('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/'
+                    + 'Data/'+ID+'/repro/source_1_ext.reg')
+    gc.show_regions('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/'
+                    + 'Data/'+ID+'/repro/back.reg')
+
+    radius = dist*u.kpc*astropy.cosmology.arcsec_per_kpc_comoving(z)/u.arcsec
+    radius = radius.value
+    print radius
+    if radius < 60.0:
+        radius = 60.0
+    elif radius > 150.0:
+        radius = 150.0
+    radius += 5.0
+
+    scale = 10.0*u.kpc/astropy.cosmology.arcsec_per_kpc_comoving(z)
+    scale = scale.value
+    gc.add_label(0.20, 0.1, 'ObsID: '+ID+'\n'+name, relative=True,
+                 weight='heavy', size='x-large', color='gray')
+
+    xpix, ypix = coord_source(ID)
+    hdulist = pyfits.open('/Users/andyscanzio/Documents/ETH/Semestri/'
+                          + 'FS2014/Project/Data/'+ID+'/repro/acisf'
+                          + ID+'_repro_evt2.fits')
+    wcs = pywcs.WCS(hdulist[0].header)
+    radec = wcs.wcs_pix2sky([[xpix, ypix]], 0)
+    ra, dec = radec[0][0], radec[0][1]
+
+    gc.recenter(ra, dec, radius=radius/3600.0)
+    gc.tick_labels.set_xformat('ddmmss')
+    gc.tick_labels.set_yformat('ddmmss')
+    gc.add_scalebar(10.0/3600.0)
+    gc.add_colorbar()
+    gc.scalebar.set_label('10.0"/'+str(scale)[0:4]+' kpc')
+    gc.scalebar.set_font(size='x-large', weight='heavy')
+    gc.scalebar.set_color('gray')
+    gc.save('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/Images/'
+            + ID+'_1_xray.eps')
+    gc.close()
+
+
+def plot_xray_2(ID, z, dist, name):
+    print ID
+    os.chdir('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/Data/'
+             + ID+'/repro/')
+    gc = aplpy.FITSFigure('aplpy_'+ID+'_0.5-8_flux.img')
+    gc.show_colorscale(cmap='gist_heat', vmin=0, vmax=1.0e-07,
+                       stretch='arcsinh', smooth=1)
+    gc.show_regions('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/'
+                    + 'Data/'+ID+'/repro/source_2.reg')
+    gc.show_regions('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/'
+                    + 'Data/'+ID+'/repro/source_2_ext.reg')
+    gc.show_regions('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/'
+                    + 'Data/'+ID+'/repro/back.reg')
+
+    radius = dist*u.kpc*astropy.cosmology.arcsec_per_kpc_comoving(z)/u.arcsec
+    radius = radius.value
+    print radius
+    if radius < 60.0:
+        radius = 60.0
+    elif radius > 150.0:
+        radius = 150.0
+    radius += 5.0
+
+    scale = 10.0*u.kpc/astropy.cosmology.arcsec_per_kpc_comoving(z)
+    scale = scale.value
+    gc.add_label(0.20, 0.1, 'ObsID: '+ID+'\n'+name, relative=True,
+                 weight='heavy', size='x-large', color='gray')
+
+    xpix, ypix = coord_source(ID)
+    hdulist = pyfits.open('/Users/andyscanzio/Documents/ETH/Semestri/'
+                          + 'FS2014/Project/Data/'+ID+'/repro/acisf'
+                          + ID+'_repro_evt2.fits')
+    wcs = pywcs.WCS(hdulist[0].header)
+    radec = wcs.wcs_pix2sky([[xpix, ypix]], 0)
+    ra, dec = radec[0][0], radec[0][1]
+
+    gc.recenter(ra, dec, radius=radius/3600.0)
+    gc.tick_labels.set_xformat('ddmmss')
+    gc.tick_labels.set_yformat('ddmmss')
+    gc.add_scalebar(10.0/3600.0)
+    gc.add_colorbar()
+    gc.scalebar.set_label('10.0"/'+str(scale)[0:4]+' kpc')
+    gc.scalebar.set_font(size='x-large', weight='heavy')
+    gc.scalebar.set_color('gray')
+    gc.save('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/Images/'
+            + ID+'_2_xray.eps')
+    gc.close()
+
+
+def plot_optical(ID, z, dist, name):
+    print ID
+    gc = aplpy.FITSFigure(ID+'_optical.fits')
+    gc.show_grayscale()
+
+    xpix, ypix = coord_source(ID)
+    hdulist = pyfits.open('/Users/andyscanzio/Documents/ETH/Semestri/'
+                          + 'FS2014/Project/Data/'+ID+'/repro/acisf'
+                          + ID+'_repro_evt2.fits')
+    wcs = pywcs.WCS(hdulist[0].header)
+    radec = wcs.wcs_pix2sky([[xpix, ypix]], 0)
+    ra, dec = radec[0][0], radec[0][1]
+
+    gc.add_label(0.20, 0.1, 'ObsID: '+ID+'\n'+name, relative=True,
+                 weight='heavy', size='x-large', color='gray')
+
+    radius = dist*u.kpc*astropy.cosmology.arcsec_per_kpc_comoving(z)/u.arcsec
+    radius = radius.value
+    print radius
+    if radius < 60.0:
+        radius = 60.0
+    elif radius > 150.0:
+        radius = 150.0
+    radius += 5.0
+
+    scale = 10.0*u.kpc/astropy.cosmology.arcsec_per_kpc_comoving(z)
+    scale = scale.value
+
+    gc.recenter(ra, dec, radius=radius/3600.0)
+    gc.tick_labels.set_xformat('ddmmss')
+    gc.tick_labels.set_yformat('ddmmss')
+    gc.add_scalebar(10.0/3600.0)
+
+    gc.scalebar.set_label('10.0"/'+str(scale)[0:4]+' kpc')
+    gc.scalebar.set_font(size='x-large', weight='heavy')
+    gc.scalebar.set_color('gray')
+    gc.save('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/Images/'
+            + ID+'_optical.eps')
+    gc.close()
+
+
+def plot_optical_1(ID, z, dist, name):
+    print ID
+    gc = aplpy.FITSFigure(ID+'_optical.fits')
+    gc.show_grayscale()
+
+    xpix, ypix = coord_source(ID)
+    hdulist = pyfits.open('/Users/andyscanzio/Documents/ETH/Semestri/'
+                          + 'FS2014/Project/Data/'+ID+'/repro/acisf'
+                          + ID+'_repro_evt2.fits')
+    wcs = pywcs.WCS(hdulist[0].header)
+    radec = wcs.wcs_pix2sky([[xpix, ypix]], 0)
+    ra, dec = radec[0][0], radec[0][1]
+
+    gc.add_label(0.20, 0.1, 'ObsID: '+ID+'\n'+name, relative=True,
+                 weight='heavy', size='x-large', color='gray')
+
+    radius = dist*u.kpc*astropy.cosmology.arcsec_per_kpc_comoving(z)/u.arcsec
+    radius = radius.value
+    print radius
+    if radius < 60.0:
+        radius = 60.0
+    elif radius > 150.0:
+        radius = 150.0
+    radius += 5.0
+
+    scale = 10.0*u.kpc/astropy.cosmology.arcsec_per_kpc_comoving(z)
+    scale = scale.value
+
+    gc.recenter(ra, dec, radius=radius/3600.0)
+    gc.tick_labels.set_xformat('ddmmss')
+    gc.tick_labels.set_yformat('ddmmss')
+    gc.add_scalebar(10.0/3600.0)
+
+    gc.scalebar.set_label('10.0"/'+str(scale)[0:4]+' kpc')
+    gc.scalebar.set_font(size='x-large', weight='heavy')
+    gc.scalebar.set_color('gray')
+    gc.save('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/Images/'
+            + ID+'_1_optical.eps')
+    gc.close()
+
+
+def plot_optical_2(ID, z, dist, name):
+    print ID
+    gc = aplpy.FITSFigure(ID+'_optical.fits')
+    gc.show_grayscale()
+
+    xpix, ypix = coord_source(ID)
+    hdulist = pyfits.open('/Users/andyscanzio/Documents/ETH/Semestri/'
+                          + 'FS2014/Project/Data/'+ID+'/repro/acisf'
+                          + ID+'_repro_evt2.fits')
+    wcs = pywcs.WCS(hdulist[0].header)
+    radec = wcs.wcs_pix2sky([[xpix, ypix]], 0)
+    ra, dec = radec[0][0], radec[0][1]
+
+    gc.add_label(0.20, 0.1, 'ObsID: '+ID+'\n'+name, relative=True,
+                 weight='heavy', size='x-large', color='gray')
+
+    radius = dist*u.kpc*astropy.cosmology.arcsec_per_kpc_comoving(z)/u.arcsec
+    radius = radius.value
+    print radius
+    if radius < 60.0:
+        radius = 60.0
+    elif radius > 150.0:
+        radius = 150.0
+    radius += 5.0
+
+    scale = 10.0*u.kpc/astropy.cosmology.arcsec_per_kpc_comoving(z)
+    scale = scale.value
+
+    gc.recenter(ra, dec, radius=radius/3600.0)
+    gc.tick_labels.set_xformat('ddmmss')
+    gc.tick_labels.set_yformat('ddmmss')
+    gc.add_scalebar(10.0/3600.0)
+
+    gc.scalebar.set_label('10.0"/'+str(scale)[0:4]+' kpc')
+    gc.scalebar.set_font(size='x-large', weight='heavy')
+    gc.scalebar.set_color('gray')
+    gc.save('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/Images/'
+            + ID+'_2_optical.eps')
+    gc.close()
+
+
+def area_spec(ID, z):
+    os.chdir('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/Data/'
+             + ID[0:5]+'/repro/')
+    area = pyfits.getval(ID+'_spec_ext.pi', 'backscal', ext=1)*8192**2
+    area = area*0.25*u.arcsec**2
+    area = area/(astropy.cosmology.arcsec_per_kpc_comoving(z)**2)
+    area = float(area/u.kpc**2)
+    return area
---
+++ semproj_xspec.py
@@ -0,0 +1,214 @@
+import os
+from xspec import *
+import subprocess
+from numpy import *
+
+
+def det_hr(s1):
+    s1.ignore("**-0.5")
+    s1.ignore("2.0-**")
+    s = s1.rate[0]*s1.exposure
+    s1.ignore("**-2.0")
+    s1.notice("2.0-8.0")
+    h = s1.rate[0]*s1.exposure
+    HR = (h-s)/(h+s)
+    s1.notice("0.5-8.0")
+    errHR = sqrt((2*s*sqrt(h))**2+(2*h*sqrt(s))**2)/((h+s)**2)
+    return HR, errHR
+
+
+def getnh(ra, dec):
+    output = subprocess.check_output("nh equinox=2000 ra="+str(ra)+" dec="
+                                     + str(dec)+" | grep Weighted",
+                                     shell=True)
+    nhgal = float(output.split()[6])
+    nhxspec = nhgal/10.**22
+    return nhgal, nhxspec
+
+
+def plot_spec(ID, name, ra, dec, z):
+
+    os.chdir('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/Data/'
+             + ID[0:5]+'/repro')
+
+    #Import spectrum
+    s = Spectrum(ID+'_spec_grp.pi')
+    counts = int(s.rate[0]*s.exposure)
+    rate = s.rate[0]
+    HR, errHR = det_hr(s)
+
+    #Set model
+    m = Model('phabs*zphabs*zpow')
+
+    #Plotting setting
+    Plot.xAxis = "keV"
+    AllData.ignore('**-0.5, 8.0-**')
+    if counts < 250:
+        Plot.setRebin(0.1, 30)
+    else:
+        Plot.setRebin(10, 30)
+
+    #nh
+    nhgal, nhxspec = getnh(ra, dec)
+
+    #Parameters
+    m.phabs.nH = nhxspec
+    m.phabs.nH.frozen = True
+    m.zphabs.Redshift = z
+    m.zpowerlw.Redshift = z
+    m.zpowerlw.PhoIndex.values = [2, 0.01, 1.5, 1.7, 2.1, 2.3]
+
+    #Fit
+    Fit.statMethod = "cstat 1"
+    Fit.query = "yes"
+    Fit.nIterations = 50
+    Fit.perform()
+
+    #Strings
+    nh = m.zphabs.nH.values[0]+m.phabs.nH.values[0]
+    nherr = m.zphabs.nH.sigma+m.phabs.nH.values[0]
+    photon = m.zpowerlw.PhoIndex.values[0]
+    photonerr = m.zpowerlw.PhoIndex.sigma
+    NH = 'NH = ('+str(nh)[0:4]+')e22'+'cm^-2'
+    phot = 'Photon index = '+str(photon)[0:3]
+    if Fit.dof == 0:
+        redchi = 0
+    else:
+        redchi = Fit.statistic/Fit.dof
+    hr = 'HR = '+str(HR)[0:4]
+    chi = 'Red Chi^2 = '+str(redchi)[0:3]
+    title = 'label top \"Observation ID: '+ID+' - '+name
+    stuff = 'label f \"'+NH+'\n'+phot+'\n'+hr+'\n'+chi
+
+    #Plot
+    Plot.device = '/Users/andyscanzio/Documents/ETH/Semestri/'
+    +'FS2014/Project/Plots/'+ID+'_counts.ps/cps'
+    Plot.addCommand(title)
+    Plot.addCommand(stuff)
+    Plot('ldata residuals')
+    Plot.device = 'none'
+
+    AllData.clear()
+
+    return counts, rate, HR, errHR, nh, nherr, photon, photonerr, redchi
+
+
+def plot_spec_ext(ID, name, ra, dec, z):
+
+    os.chdir('/Users/andyscanzio/Documents/ETH/Semestri/FS2014/Project/Data/'
+             + ID[0:5]+'/repro')
+
+    #Import spectrum
+    s = Spectrum(ID+'_spec_ext_grp.pi')
+    counts = int(s.rate[0]*s.exposure)
+    rate = s.rate[0]
+
+    #Set model
+    m = Model('phabs*zpow')
+
+    #Plotting setting
+    Plot.xAxis = "keV"
+    AllData.ignore('**-0.5, 8.0-**')
+    if counts < 250:
+        Plot.setRebin(0.1, 30)
+    else:
+        Plot.setRebin(10, 30)
+
+    #nh
+    nhgal, nhxspec = getnh(ra, dec)
+
+    #Parameters
+    m.phabs.nH = nhxspec
+    m.phabs.nH.frozen = True
+    m.zpowerlw.Redshift = z
+
+    #Fit
+    Fit.statMethod = "cstat 1"
+    Fit.query = "yes"
+    Fit.nIterations = 50
+    Fit.perform()
+
+    #Strings
+    photon = m.zpowerlw.PhoIndex.values[0]
+    photonerr = m.zpowerlw.PhoIndex.sigma
+    phot = 'Photon index = '+str(photon)[0:3]
+    if Fit.dof == 0:
+        redchi = 0
+    else:
+        redchi = Fit.statistic/Fit.dof
+    chi = 'Red Chi^2 = '+str(redchi)[0:3]
+    title = 'label top \"Observation ID: '+ID+' - '+name
+    stuff = 'label f \"'+phot+'\n'+chi
+
+    #Plot
+    Plot.device = '/Users/andyscanzio/Documents/ETH/Semestri/'
+    + 'FS2014/Project/Plots/'+ID+'_counts_ext.ps/cps'
+    Plot.addCommand(title)
+    Plot.addCommand(stuff)
+    Plot('ldata residuals')
+    Plot.device = 'none'
+
+    AllData.clear()
+
+    return counts, rate, photon, photonerr, redchi
+
+
+def output(ID, name, res):
+    os.chdir('/Users/andyscanzio/Documents/ETH/Semestri/'
+             + 'FS2014/Project/Outputs/')
+    with open(ID+'_output.txt', 'w') as f:
+        f.write('ObsID: '+ID+'\n')
+        f.write('Name: '+name+'\n')
+        f.write('Total count = '+str(res[0])+'\n')
+        f.write('Counts per second = '+str(res[1])+'\n')
+        f.write('Hardness ratio = '+str(res[2])[0:5]+' +/- '
+                + str(res[3])[0:5]+'\n')
+        f.write('NH = ('+str(res[4])[0:5]+' +/- '
+                + str(res[5])[0:5]+')e22'+'cm^-2\n')
+        f.write('Photon index = '+str(res[6])[0:5]+' +/- '
+                + str(res[7])[0:5]+'\n')
+        f.write('Reduced chi-squared = '+str(res[8]))
+        f.write('\n')
+        f.write('\n')
+
+
+def output_ext(ID, name, res_ext):
+    os.chdir('/Users/andyscanzio/Documents/ETH/Semestri/'
+             + 'FS2014/Project/Outputs/')
+    with open(ID+'_output_ext.txt', 'w') as f:
+        f.write('ObsID: '+ID+'\n')
+        f.write('Name: '+name+'\n')
+        f.write('Total count = '+str(res_ext[0])+'\n')
+        f.write('Counts per second = '+str(res_ext[1])+'\n')
+        f.write('Photon index = '+str(res_ext[2])[0:5]+' +/- '
+                + str(res_ext[3])[0:5]+'\n')
+        f.write('Reduced chi-squared = '+str(res_ext[4]))
+        f.write('\n')
+        f.write('\n')
+
+
+def HR(ID):
+    os.chdir('/Users/andyscanzio/Documents/ETH/Semestri/'
+             + 'FS2014/Project/Data/'+ID[0:5]+'/repro')
+    s = Spectrum(ID+'_spec_grp.pi')
+    HR, errHR = det_hr(s)
+    AllData.clear()
+    return HR, errHR
+
+
+def cps_ext(ID):
+    os.chdir('/Users/andyscanzio/Documents/ETH/Semestri/'
+             + 'FS2014/Project/Data/'+ID[0:5]+'/repro')
+    s = Spectrum(ID+'_spec_ext_grp.pi')
+    rate = s.rate[0]
+    err_rate = 0
+    return rate,  err_rate
+
+
+def cps(ID):
+    os.chdir('/Users/andyscanzio/Documents/ETH/Semestri/'
+             + 'FS2014/Project/Data/'+ID[0:5]+'/repro')
+    s = Spectrum(ID+'_spec_grp.pi')
+    rate = s.rate[0]
+    err_rate = 0
+    return rate,  err_rate
